622ab1de5068bcbc3584b49f9942a8e0f77678e6
Froggy <frogscooper@gmail.com>
2025-01-31 14:42:25 -0500

    added some barebones shitcode. will eventually add a file selection window instead of just asking you to manually type the entire path

--- catboytorrent.py
+++ catboytorrent.py
@@ -1,4 +1,21 @@
 import bencodepy
 
-##Testing
-print(bencodepy.encode(123))
+## Makes sure that the file ends in .torrent
+
+
+while True:
+    torrent_path = input("Please input the path of your torrent file > ")
+    if torrent_path.endswith('.torrent') == False:
+        print("Please select a file that ends in .torrent")
+    else:
+        print("Getting started! :)")
+        break
+    
+
+##Reads and decodes the torrent file
+
+with open(torrent_path, 'rb') as file:
+    meta_info = file.read()
+    torrent = bencodepy.decode(meta_info)
+print(torrent)
+
