82f1fecc57f99b7dd0c44f56612902ff428b741e
Froggy <frogscooper@gmail.com>
2025-02-02 17:16:52 -0500

    added a peer ID for connecting to the tracker. i think there is a better way to do the random_peer_piece but i am not good enough at syntax to know a better solution right now

--- catboytorrent.py
+++ catboytorrent.py
@@ -1,7 +1,10 @@
 import bencodepy
+import requests
+import random
+
+version_ID = ("0000")
 
 ## Makes sure that the file ends in .torrent
-
 
 while True:
     torrent_path = input("Please input the path of your torrent file > ")
@@ -17,5 +20,9 @@ while True:
 with open(torrent_path, 'rb') as file:
     meta_info = file.read()
     torrent = bencodepy.decode(meta_info)
-print(torrent)
 
+##Generate the peer ID, which is normally formatted as <2 digits to represent client ID> + <4 digits to represent Client version> + <12 random digits>
+random.seed()
+random_peer_part = random.randint(0, 999999999999)
+peer_ID = (f"CB{version_ID}{random_peer_part}")
+print(peer_ID)
