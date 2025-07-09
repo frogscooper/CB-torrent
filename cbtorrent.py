08200430eb24b12a7c1b0d646663f02baf33b81f
frogscooper <frogscooper@gmail.com>
2025-07-08 21:52:09 -0400

    Add files via upload
    Fix formatting for version ID

--- cbtorrent.py
+++ cbtorrent.py
@@ -1,3 +1,4 @@
+import collections
 import bencodepy
 import requests
 import random
@@ -23,10 +24,13 @@ while True:
 
 with open(torrent_file_path, 'rb') as file:
     content = file.read()
-    torrent = bencodepy.decode(content)
+    decoded_file = bencodepy.decode(content)
+f isinstance(decoded_file, (collections.OrderedDict)) == False:
+#    print("It looks like there's a problem with your torrent file.")
+#print(type(decoded_file)) 
 
-##Generate the peer ID, which is normally formatted as <2 digits to represent client ID> + <4 digits to represent Client version> + <12 random digits>
+##Generate the peer ID, which is normally formatted as <2 digits to represent client ID> + <4 digits to represent Client version> + <-> + <12 random digits>
 random.seed()
 random_peer_part = random.randint(0, 999999999999)
-peer_ID = (f"CB{version_ID}{random_peer_part}")
+peer_ID = (f"CB{version_ID}-{random_peer_part}")
 print(peer_ID)
