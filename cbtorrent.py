a537165b55008c7ddc69606d74acc1a2b2019646
frogscooper <frogscooper@gmail.com>
2025-08-07 19:21:10 -0400

    Add files via upload
    fixed formatting error in peer ID and working on building HTTP GET request to connect to tracker

--- cbtorrent.py
+++ cbtorrent.py
@@ -24,9 +24,10 @@ with open(torrent_file_path, 'rb') as file:
     content = file.read()
     decoded_file = bencodepy.decode(content) 
 
+print(decoded_file)
 ##Generate the peer ID, which is normally formatted as <2 digits to represent client ID> + <4 digits to represent Client version> + <-> + <12 random digits>
 random.seed()
-peer_ID = (f"CB{0000}-{random.randint(0, 999999999999}")
+peer_ID = (f"CB0000-{random.randint(0, 999999999999)}")
 print(peer_ID)
 
-
+#tracker = requests.get
